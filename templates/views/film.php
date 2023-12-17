8fd8df2d8e03824d4742080606aca09ba7abcfdb
NathanMarques2001 <nathanmarques20@yahoo.com.br>
2023-12-17 00:49:35 -0300

    Add film details and styling improvements

--- templates/views/film.php
+++ templates/views/film.php
@@ -10,24 +10,33 @@ $filmID = $parts[2];
 $film = $tmdb->getMovie($filmID);
 $providers = $tmdb->getProviders($filmID);
 $streamings = $providers->results->BR->flatrate;
+$year = substr($film->release_date, 0, 4);
+
+$genreNames = array_map(function ($genre) {
+  return $genre->name;
+}, $film->genres);
+
+$genres = implode(', ', $genreNames);
+
+$streamingsName = array_map(function ($streaming) {
+  return $streaming->provider_name;
+}, $providers->results->BR->flatrate);
+
+$streamings = implode(', ', $streamingsName);
 ?>
 
 <div id="filmDetail-container" style="background-image: url('<?= $tmdb->getBackground($film->backdrop_path); ?>');">
   <div id="filmDetail-content">
-    <h1><?= $film->title ?></h1>
-    <img src="<?= $tmdb->getImage($film->poster_path); ?>" alt="">
-    <p><?= $film->overview ?></p>
-    <p>Gêneros</p>
-    <ul>
-      <?php foreach ($film->genres as $genre) : ?>
-        <li><?= $genre->name ?></li>
-      <?php endforeach; ?>
-    </ul>
-    <p>Streamings</p>
-    <ul>
-      <?php foreach ($streamings as $streaming) : ?>
-        <li><?= $streaming->provider_name ?></li>
-      <?php endforeach; ?>
-    </ul>
+    <img src="<?= $tmdb->getImage($film->poster_path); ?>" alt="Pôster <?= $film->title ?>" id="filmDetail-img">
+    <div>
+      <h1><?= $film->title ?> &lpar;<?= $year ?>&rpar;</h1>
+      <p><?= $genres ?></p>
+      <p>Avaliação -> <?= round($film->vote_average * 10) ?>%</p>
+      <p><?= $film->tagline ?></p>
+      <p>Sinopse</p>
+      <p><?= $film->overview ?></p>
+      <p>Streamings</p>
+      <p><?= $streamings ?></p>
+    </div>
   </div>
 </div>
