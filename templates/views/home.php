8ddf2c133d5fcfaa834ccce2d40933c1cfe17330
NathanMarques2001 <nathanmarques20@yahoo.com.br>
2024-01-20 12:51:35 -0300

    Estilo home page #4

--- templates/views/home.php
+++ templates/views/home.php
@@ -9,24 +9,24 @@
         <div class="card" style="margin-right: 1rem;">
           <img src="<?= $tmdb->getImage($film->getImagePath()); ?>" class="card-img-top" alt="Pôster - <?= $film->getTitle() ?>">
           <div class="card-body">
-            <h5 class="card-title"><?= $film->getTitle() ?></h5>
+            <h5 class="card-title" style="min-height: 4rem; display:flex;justify-content:center;align-items:center;"><?= $film->getTitle() ?></h5>
             <p class="card-text" style="height: 320px; overflow: auto;"><?= $film->getOverview() ?></p>
-            <a href="http://moviematch.com/movie-detail/<?= $film->getID() ?>" class="btn btn-primary">Mais Detalhes</a>
+            <a href="http://moviematch.com/movie-detail/<?= $film->getID() ?>" class="btn btn-info" style="width:95%;">Mais Detalhes</a>
             <form action="http://moviematch.com/add-list" method="POST">
               <input type="hidden" name="film_id" value="<?= $film->getId() ?>">
-              <button class="btn btn-primary">Adicionar a lista</button>
+              <button class="btn btn-success" style="width:95%;">Adicionar a lista</button>
             </form>
             <form action="http://moviematch.com/not-interested" method="POST">
               <input type="hidden" name="film_id" value="<?= $film->getId() ?>">
               <input type="hidden" name="film_overview" value="<?= $film->getOverview() ?>">
-              <button class="btn btn-primary">Não me interessa</button>
+              <button class="btn btn-danger" style="width:95%;">Não me interessa</button>
             </form>
           </div>
         </div>
       <?php endforeach; ?>
     </main>
     <form action="http://moviematch.com/home" method="POST">
-      <button type="submit" class="btn btn-primary btn-lg mb-3" name="changeMovies">Novas recomendações</button>
+      <button type="submit" class="btn btn-primary btn-lg mb-5" name="changeMovies" style="width:60%;">Novas recomendações</button>
     </form>
   </div>
 </body>
